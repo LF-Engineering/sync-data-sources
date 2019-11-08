855e40f7091e92630816929af02ed81133261fc4
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-11-08 18:30:57 +0000

    one more fix
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- docker-images/scripts/unzip.sh
+++ docker-images/scripts/unzip.sh
@@ -4,4 +4,8 @@ then
   echo "$0: you have to specify unzip password via ZIPPASS=..."
   exit 1
 fi
-unzip -P "$ZIPPASS" data.zip > /dev/null && rm -f data.zip
+
+if [ -f "data.zip" ]
+then
+  unzip -P "$ZIPPASS" data.zip > /dev/null && rm -f data.zip
+fi
