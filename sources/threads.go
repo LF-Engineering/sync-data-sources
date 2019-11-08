1a82e58a1a2e95083a5c6774f67ef031f38b1b1b
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-11-08 10:40:20 +0000

    Added threads lib
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ sources/threads.go
@@ -0,0 +1,26 @@
+package syncdatasources
+
+import (
+	"runtime"
+)
+
+// GetThreadsNum returns the number of available CPUs
+// If environment variable GHA2DB_ST is set it retuns 1
+// It can be used to debug single threaded verion
+func GetThreadsNum(ctx *Ctx) int {
+	// Use environment variable to have singlethreaded version
+	if ctx.NCPUs > 0 {
+		n := runtime.NumCPU()
+		if ctx.NCPUs > n {
+			ctx.NCPUs = n
+		}
+		runtime.GOMAXPROCS(ctx.NCPUs)
+		return ctx.NCPUs
+	}
+	if ctx.ST {
+		return 1
+	}
+	thrN := runtime.NumCPU()
+	runtime.GOMAXPROCS(thrN)
+	return thrN
+}
