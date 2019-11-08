65c796d6e86e9a6f577c119a161a45ff19ffa915
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-11-08 17:16:24 +0000

    handle datasources YAML struct in go
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- sources/fixture.go
+++ sources/fixture.go
@@ -1,7 +1,13 @@
 package syncdatasources
 
+// DataSource contains data source spec from dev-analytics-api
+type DataSource struct {
+	Slug string `yaml:"slug"`
+}
+
 // Fixture contains full YAML structure of dev-analytics-api fixture files
 type Fixture struct {
-	Native map[string]string `yaml:"native"`
-	Fn     string
+	Native      map[string]string `yaml:"native"`
+	DataSources []DataSource      `yaml:"data_sources"`
+	Fn          string
 }
