863b8435d0c6bb4750407e40d99eebee1e2466d2
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-11-08 18:29:28 +0000

    Remove non-needed file
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- docker-images/scripts/unzip.sh
+++ docker-images/scripts/unzip.sh
@@ -4,4 +4,4 @@ then
   echo "$0: you have to specify unzip password via ZIPPASS=..."
   exit 1
 fi
-unzip -P "$ZIPPASS" data.zip > /dev/null
+unzip -P "$ZIPPASS" data.zip > /dev/null && rm -f data.zip
