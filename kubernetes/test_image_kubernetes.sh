2602088b9d74f87a71604fd7e9fbb502c8127fd0
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-11-08 06:12:41 +0000

    Kubernetes test option
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ kubernetes/test_image_kubernetes.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+if [ -z "${DOCKER_USER}" ]
+then
+  echo "$0: you need to set docker user via DOCKER_USER=username"
+  exit 2
+fi
+if [ -z "${BRANCH}" ]
+then
+  echo "$0: you need to set dev-analytics-branch via BRANCH=test|prod"
+  exit 2
+fi
+command="$1"
+if [ -z "${command}" ]
+then
+  export command=/bin/bash
+fi
+ts=`date +'%s%N'`
+kubectl run -i --tty "sync-data-sources-${BRANCH}-test-${ts}" --restart=Never --rm --image="${DOCKER_USER}/sync-data-sources-${sync-data-sources}" --command "$command"
