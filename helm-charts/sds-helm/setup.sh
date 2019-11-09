2b6bc019d6a6ae7fdab75bdfc063cd23af097053
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-11-09 15:12:33 +0000

    Chart updates
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- helm-charts/sds-helm/setup.sh
+++ helm-charts/sds-helm/setup.sh
@@ -6,7 +6,5 @@ then
 fi
 "${1}h.sh" install sds-namespace ./sds-helm --set "skipSecrets=1,skipCron=1,skipPV=1"
 change_namespace.sh $1 sds
-"${1}h.sh" install sds-secrets ./sds-helm --set "deployEnv=$1,skipCron=1,skipNamespace=1,skipPV=1"
-"${1}h.sh" install sds-pv ./sds-helm --set "deployEnv=$1,skipCron=1,skipNamespace=1,skipSecrets=1"
-"${1}h.sh" install sds-cronjob ./sds-helm --set "deployEnv=$1,skipNamespace=1,skipSecrets=1,skipPV=1"
+"${1}h.sh" install sds ./sds-helm --set "deployEnv=$1,skipNamespace=1"
 change_namespace.sh $1 default
