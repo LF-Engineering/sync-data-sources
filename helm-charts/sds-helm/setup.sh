c7734ff47334d6867e4e62c808c8bc46a5f52f68
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-11-10 11:28:50 +0000

    Fix setup script
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- helm-charts/sds-helm/setup.sh
+++ helm-charts/sds-helm/setup.sh
@@ -8,5 +8,5 @@ then
 fi
 "${1}h.sh" install sds-namespace ./sds-helm --set "skipSecrets=1,skipCron=1,skipPV=1"
 change_namespace.sh $1 sds
-"${1}h.sh" install sds ./sds-helm --set "deployEnv=$1,skipNamespace=1,debugPod='$DBG',esBulkSize='$ES_BULK_SIZE'"
+"${1}h.sh" install sds ./sds-helm --set "deployEnv=$1,skipNamespace=1,debugPod=$DBG,esBulkSize=$ES_BULK_SIZE"
 change_namespace.sh $1 default
