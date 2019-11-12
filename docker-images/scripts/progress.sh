f32c829609dd59deae39a99a4545c8dc27d99e48
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-11-12 08:23:32 +0000

    Fix progress script [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- docker-images/scripts/progress.sh
+++ docker-images/scripts/progress.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pid=`ps | grep syncdatasources | head -n 1 | awk '{print $1}'`
+pid=`ps -a | grep syncdatasources | head -n 1 | awk '{print $1}'`
 if [ ! -z "$pid" ]
 then
   kill -SIGUSR1 "$pid"
