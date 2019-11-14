e4a41ccd6db3f12f6e1352b71954b5a1e886c88d
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-11-14 04:42:15 +0000

    Adding multiple nodes support to helm chart
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- helm-charts/sds-helm/setup.sh
+++ helm-charts/sds-helm/setup.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 # DBG=1 deploy with debug pod installed
 # ES_BULK_SIZE=10000 - set ES bulk size
+# NODES=4 - set number of nodes
+# DRY=1 - dry run mode
 if [ -z "$1" ]
 then
   echo "$0: you need to specify env: test, dev, stg, prod"
   exit 1
 fi
-"${1}h.sh" install sds-namespace ./sds-helm --set "skipSecrets=1,skipCron=1,skipPV=1"
-change_namespace.sh $1 sds
-"${1}h.sh" install sds ./sds-helm --set "deployEnv=$1,skipNamespace=1,debugPod=$DBG,esBulkSize=$ES_BULK_SIZE"
-change_namespace.sh $1 default
+if [ -z "$NODES" ]
+then
+  export NODES=1
+fi
+if [ -z "$DRY" ]
+then
+  "${1}h.sh" install sds-namespace ./sds-helm --set "skipSecrets=1,skipCron=1,skipPV=1,nodeNum=$NODES"
+  change_namespace.sh $1 sds
+  "${1}h.sh" install sds ./sds-helm --set "deployEnv=$1,skipNamespace=1,debugPod=$DBG,esBulkSize=$ES_BULK_SIZE,nodeNUM=$Nodes"
+  change_namespace.sh $1 default
+else
+  "${1}h.sh" install --debug --dry-run ./sds-helm --set "deployEnv=$1,debugPod=$DBG,esBulkSize=$ES_BULK_SIZE,nodeNUM=$Nodes"
+fi
