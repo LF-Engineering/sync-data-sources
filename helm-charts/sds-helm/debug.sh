b8677aa236401dd7b1dad5bbc984814b51a393d2
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-12-04 04:59:16 +0000

    More debugging options
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ helm-charts/sds-helm/debug.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if [ -z "$1" ]
+then
+  echo "$0: you need to specify env: test, dev, stg, prod"
+  exit 1
+fi
+change_namespace.sh $1 sds
+"${1}h.sh" install sds-debug ./sds-helm --set deployEnv="$1",skipSecrets=1,skipCron=1,skipNamespace=1,skipPV=1,debugPod=1
+change_namespace.sh $1 default
