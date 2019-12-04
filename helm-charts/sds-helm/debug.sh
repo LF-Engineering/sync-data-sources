efecf10636c024d9d83fd40f085bce2ad4851cda
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-12-04 08:11:31 +0000

    Handle setting p2o.py error size cut level
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- helm-charts/sds-helm/debug.sh
+++ helm-charts/sds-helm/debug.sh
@@ -5,5 +5,5 @@ then
   exit 1
 fi
 change_namespace.sh $1 sds
-"${1}h.sh" install sds-debug ./sds-helm --set deployEnv="$1",skipSecrets=1,skipCron=1,skipNamespace=1,skipPV=1,debugPod=1
+"${1}h.sh" install sds-debug ./sds-helm --set deployEnv="$1",skipSecrets=1,skipCron=1,skipNamespace=1,skipPV=1,debugPod=1,stripErrorSize=4096
 change_namespace.sh $1 default
