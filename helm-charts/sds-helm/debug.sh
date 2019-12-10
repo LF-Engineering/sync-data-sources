b088d86bb1d7c05edd64996e4e1c75d6823c65f0
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-12-10 07:45:08 +0000

    Added extra flags support
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- helm-charts/sds-helm/debug.sh
+++ helm-charts/sds-helm/debug.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # NODES=4 - set number of nodes
 # NS=sds - set namespace name, default sds
+# FLAGS='esURL="`cat sds-helm/secrets/ES_URL_external.secret`",pvSize=30Gi"
 if [ -z "$1" ]
 then
   echo "$0: you need to specify env: test, dev, stg, prod"
@@ -17,6 +18,10 @@ if [ -z "$NS" ]
 then
   NS=sds
 fi
+if [ -z "$FLAGS" ]
+then
+  FLAGS="foo=bar"
+fi
 change_namespace.sh $1 "$NS"
-"${1}h.sh" install "${NS}-debug" ./sds-helm --set "namespace=$NS,deployEnv="$1",skipSecrets=1,skipCron=1,skipNamespace=1,skipPV=1,debugPod=1,stripErrorSize=4096,nodeNum=$NODES,nodeHash=$HSH"
+"${1}h.sh" install "${NS}-debug" ./sds-helm --set "namespace=$NS,deployEnv="$1",skipSecrets=1,skipCron=1,skipNamespace=1,skipPV=1,debugPod=1,stripErrorSize=4096,nodeNum=$NODES,nodeHash=$HSH,$FLAGS"
 change_namespace.sh $1 default
