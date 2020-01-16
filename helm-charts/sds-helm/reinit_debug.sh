6b50fdf6e8950569e8d6e389d6dc44cbb643eaf5
lukaszgryglicki <lukaszgryglicki@o2.pl>
2020-01-16 09:22:14 +0000

    Change mode
    Signed-off-by: lukaszgryglicki <lukaszgryglicki@o2.pl>

--- helm-charts/sds-helm/reinit_debug.sh
+++ helm-charts/sds-helm/reinit_debug.sh
@@ -1,5 +1,6 @@
+#!/bin/bash
 NS=sds-ext ./debug_delete.sh prod
 NS=sds-ext ./debug_delete.sh test
-sleep 30
+sleep 60
 NS=sds-ext NODES=2 FLAGS=esURL=`cat sds-helm/secrets/ES_URL_Fayaz_prod.secret` ./debug.sh prod
 NS=sds-ext NODES=2 FLAGS=esURL=`cat sds-helm/secrets/ES_URL_Fayaz_test.secret` ./debug.sh test
