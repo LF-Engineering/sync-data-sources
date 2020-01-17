4a79310e3353fd75a61843e1f7b0ad2b4de9931b
lukaszgryglicki <lukaszgryglicki@o2.pl>
2020-01-17 19:41:34 +0000

    Update debug pods installator
    Signed-off-by: lukaszgryglicki <lukaszgryglicki@o2.pl>

--- helm-charts/sds-helm/reinit_debug.sh
+++ helm-charts/sds-helm/reinit_debug.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 NS=sds-ext ./debug_delete.sh prod
 NS=sds-ext ./debug_delete.sh test
+NS=sds ./debug_delete.sh prod
+NS=sds ./debug_delete.sh test
 sleep 60
 NS=sds-ext NODES=2 FLAGS=esURL=`cat sds-helm/secrets/ES_URL_Fayaz_prod.secret` ./debug.sh prod
 NS=sds-ext NODES=2 FLAGS=esURL=`cat sds-helm/secrets/ES_URL_Fayaz_test.secret` ./debug.sh test
+NS=sds NODES=2 ./debug.sh prod
+NS=sds NODES=2 ./debug.sh test
