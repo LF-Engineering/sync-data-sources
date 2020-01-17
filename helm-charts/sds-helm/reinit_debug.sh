dc7a4b5b6bdf24aa3b86c42708af23a8ff110ef0
lukaszgryglicki <lukaszgryglicki@o2.pl>
2020-01-17 19:43:23 +0000

    More updates to script
    Signed-off-by: lukaszgryglicki <lukaszgryglicki@o2.pl>

--- helm-charts/sds-helm/reinit_debug.sh
+++ helm-charts/sds-helm/reinit_debug.sh
@@ -4,6 +4,10 @@ NS=sds-ext ./debug_delete.sh test
 NS=sds ./debug_delete.sh prod
 NS=sds ./debug_delete.sh test
 sleep 60
+prodk.sh -n sds-ext get po
+testk.sh -n sds-ext get po
+prodk.sh -n sds get po
+testk.sh -n sds get po
 NS=sds-ext NODES=2 FLAGS=esURL=`cat sds-helm/secrets/ES_URL_Fayaz_prod.secret` ./debug.sh prod
 NS=sds-ext NODES=2 FLAGS=esURL=`cat sds-helm/secrets/ES_URL_Fayaz_test.secret` ./debug.sh test
 NS=sds NODES=2 ./debug.sh prod
