2da9178a9620a6b0b3dbf2727f43dcfcbc0e4a2d
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-03-05 05:50:43 +0000

    Script updates, docs updates and new script
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- docker-images/manual_docker.sh
+++ docker-images/manual_docker.sh
@@ -9,4 +9,9 @@ if [ ! -z "$2" ]
 then
   cmd="$2"
 fi
-docker run -e BRANCH=prod -e SDS_ES_URL=`cat helm-charts/sds-helm/sds-helm/secrets/ES_URL.$1.secret` -e SH_HOST=`cat helm-charts/sds-helm/sds-helm/secrets/SH_HOST.$1.secret` -e SH_PORT=`cat helm-charts/sds-helm/sds-helm/secrets/SH_PORT.$1.secret` -e SH_DB=`cat helm-charts/sds-helm/sds-helm/secrets/SH_DB.$1.secret` -e SH_USER=`cat helm-charts/sds-helm/sds-helm/secrets/SH_USER.$1.secret` -e SH_PASS=`cat helm-charts/sds-helm/sds-helm/secrets/SH_PASS.$1.secret` -e SDS_GITHUB_OAUTH=`cat /etc/github/oauths` -e REPO_ACCESS=`cat repo_access.secret` --rm -i -v /root/.perceval:/root/.perceval "lukaszgryglicki/sync-data-sources-prod" "$cmd"
+if [ ! -z "$SHELL" ]
+then
+  docker run -e BRANCH=prod -e SDS_ES_URL=`cat helm-charts/sds-helm/sds-helm/secrets/ES_URL.$1.secret` -e SH_HOST=`cat helm-charts/sds-helm/sds-helm/secrets/SH_HOST.$1.secret` -e SH_PORT=`cat helm-charts/sds-helm/sds-helm/secrets/SH_PORT.$1.secret` -e SH_DB=`cat helm-charts/sds-helm/sds-helm/secrets/SH_DB.$1.secret` -e SH_USER=`cat helm-charts/sds-helm/sds-helm/secrets/SH_USER.$1.secret` -e SH_PASS=`cat helm-charts/sds-helm/sds-helm/secrets/SH_PASS.$1.secret` -e SDS_GITHUB_OAUTH=`cat /etc/github/oauths` -e REPO_ACCESS=`cat repo_access.secret` --rm -i -t -v /root/.perceval:/root/.perceval "lukaszgryglicki/sync-data-sources-prod" /bin/bash
+else
+  docker run -e BRANCH=prod -e SDS_ES_URL=`cat helm-charts/sds-helm/sds-helm/secrets/ES_URL.$1.secret` -e SH_HOST=`cat helm-charts/sds-helm/sds-helm/secrets/SH_HOST.$1.secret` -e SH_PORT=`cat helm-charts/sds-helm/sds-helm/secrets/SH_PORT.$1.secret` -e SH_DB=`cat helm-charts/sds-helm/sds-helm/secrets/SH_DB.$1.secret` -e SH_USER=`cat helm-charts/sds-helm/sds-helm/secrets/SH_USER.$1.secret` -e SH_PASS=`cat helm-charts/sds-helm/sds-helm/secrets/SH_PASS.$1.secret` -e SDS_GITHUB_OAUTH=`cat /etc/github/oauths` -e REPO_ACCESS=`cat repo_access.secret` --rm -i -v /root/.perceval:/root/.perceval "lukaszgryglicki/sync-data-sources-prod" "$cmd"
+fi
