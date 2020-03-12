0f7455cbe5828ba4a643a42459df4673dbff78c8
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-03-12 07:23:27 +0000

    Update error handling, avoid returning very long p2o.py invocations
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- sources/context.go
+++ sources/context.go
@@ -39,7 +39,7 @@ type Ctx struct {
 	TimeoutSeconds      int           // From SDS_TIMEOUT_SECONDS, set entire program execution timeout, program will finish with return code 2 if anything still runs after this time, default 47 h 45 min = 171900
 	NLongest            int           // From SDS_N_LONGEST, number of longest running tasks to display in stats, default 10
 	SkipSH              bool          // From SDS_SKIP_SH, if set sorting hata database processing will be skipped
-	StripErrorSize      int           // From SDS_STRIP_ERROR_SIZE, default 1024, error messages longer that this value will be stripped by half of this value from beginning and from end, so for 1024 error 4000 bytes long will be 512 bytes from the beginning ... 512 from the end
+	StripErrorSize      int           // From SDS_STRIP_ERROR_SIZE, default 1024, error messages longer that this value will be stripped by this value from beginning and from end, so for 1024 error 4000 bytes long will be 1024 bytes from the beginning \n...\n 1024 from the end
 	GitHubOAuth         string        // From SDS_GITHUB_OAUTH, if not set it attempts to use public access, if contains "/" it will assume that it contains file name, if "," found then it will assume that this is a list of OAuth tokens instead of just one
 	LatestItems         bool          // From SDS_LATEST_ITEMS, if set pass "latest items" or similar flag to the p2o.py backend (that should be handled by p2o.py using ES, so this is probably not a good ide, git backend, for example, can return no data then)
 	CSVPrefix           string        // From SDS_CSV_PREFIX, CSV logs filename prefix, default "jobs", so files would be "/root/.perceval/jobs_I_N.csv"
