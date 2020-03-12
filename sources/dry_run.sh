689112cc0f758387fa1a0a60f8bda7516dc60551
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-03-12 13:33:10 +0000

    New dry-run default safe config
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- sources/dry_run.sh
+++ sources/dry_run.sh
@@ -4,13 +4,13 @@ then
   echo "$0: you need to specify env: test, prod"
   exit 1
 fi
-#export SDS_SKIPTIME=1
+export SDS_SKIPTIME=1
 #export SDS_SKIP_SH=1
-export SDS_SKIP_DATA=1
+#export SDS_SKIP_DATA=1
 #export SDS_SKIP_AFFS=1
-export SDS_SKIP_ALIASES=1
-export SDS_SKIP_DROP_UNUSED=1
-export SDS_SKIP_CHECK_FREQ=1
+#export SDS_SKIP_ALIASES=1
+#export SDS_SKIP_DROP_UNUSED=1
+#export SDS_SKIP_CHECK_FREQ=1
 export SDS_SKIP_ES_DATA=1
 export SDS_SKIP_ES_LOG=1
 #export SDS_SKIP_DEDUP=1
@@ -23,7 +23,7 @@ export SDS_DRY_RUN=1
 #export SDS_DRY_RUN_ALLOW_FREQ=1
 #export SDS_DRY_RUN_ALLOW_MTX=1
 #export SDS_DRY_RUN_ALLOW_RENAME=1
-export SDS_DRY_RUN_ALLOW_ORIGINS=1
+#export SDS_DRY_RUN_ALLOW_ORIGINS=1
 #export SDS_DRY_RUN_ALLOW_DEDUP=1
 export SDS_ES_URL=`cat ../helm-charts/sds-helm/sds-helm/secrets/ES_URL.$1.secret`
 export SH_HOST=`cat ../helm-charts/sds-helm/sds-helm/secrets/SH_HOST.$1.secret`
