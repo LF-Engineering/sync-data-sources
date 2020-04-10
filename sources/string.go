5d795285e9c8ec677deac387db94dfe7741719c0
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-04-10 19:02:12 +0000

    Update comment
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- sources/string.go
+++ sources/string.go
@@ -6,6 +6,8 @@ import (
 )
 
 // SafeString - return safe string without control characters and unicode correct
+// Other options would be to replace non-OK characters with "%HH" - their hexcode
+// ES would understand this
 func SafeString(str string) string {
 	isOk := func(r rune) bool {
 		return r < 32 || r >= 127
