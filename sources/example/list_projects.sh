53924f4b1c75d3b52afdc5fea08a6301dca1ab36
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-04-16 12:03:57 +0000

    Some updates to setting project via update_by_query
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- sources/example/list_projects.sh
+++ sources/example/list_projects.sh
@@ -9,4 +9,4 @@ then
   echo "$0: please specify index name as a 1st arg"
   exit 2
 fi
-curl -H 'Content-Type: application/json' "${ES_URL}/${1}/_doc/_search?size=1" -d '{"aggs":{"project":{"terms":{"field":"project", "size":2147483647}}}}' 2>/dev/null | jq '.aggregations.project.buckets'
+curl -H 'Content-Type: application/json' "${ES_URL}/${1}/_doc/_search?size=0" -d '{"aggs":{"project":{"terms":{"field":"project", "size":2147483647}}}}' 2>/dev/null | jq '.aggregations.project.buckets'
