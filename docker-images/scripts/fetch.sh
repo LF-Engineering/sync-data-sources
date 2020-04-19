a82e0c479d5350879f47160c74466b4c8f0afe62
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-04-19 08:23:57 +0000

    Only pull real branch names not commit shas
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- docker-images/scripts/fetch.sh
+++ docker-images/scripts/fetch.sh
@@ -2,6 +2,9 @@
 git clone $REPO_ACCESS || exit 1
 cd dev-analytics-api || exit 2
 git checkout $BRANCH || exit 3
-git pull || exit 4
+if ( [ "$BRANCH" = "prod" ] || [ "$BRANCH" = "test" ] || [ "$BRANCH" = "master" ] || [ "$BRANCH" = "develop" ] )
+then
+  git pull || exit 4
+fi
 cp -R app/services/lf/bootstrap/fixtures/ /data || exit 5
 cd .. && rm -rf dev-analytics-api
