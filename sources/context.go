601515666e42d31b654e182f088b1a97fa201b9c
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-05-14 18:24:20 +0000

    Change the default
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- sources/context.go
+++ sources/context.go
@@ -11,7 +11,7 @@ import (
 type Ctx struct {
 	Debug                   int           // From SDS_DEBUG Debug level: 0-no, 1-info, 2-verbose
 	CmdDebug                int           // From SDS_CMDDEBUG Commands execution Debug level: 0-no, 1-only output commands, 2-output commands and their output, 3-output full environment as well, default 0
-	MaxRetry                int           // From SDS_MAXRETRY Try to run grimoire stack (perceval, p2o.py etc) that many times before reporting failure, default 3 (1 original and 2 more attempts).
+	MaxRetry                int           // From SDS_MAXRETRY Try to run grimoire stack (perceval, p2o.py etc) that many times before reporting failure, default 0 (1 original - always runs and 0 more attempts).
 	ST                      bool          // From SDS_ST true: use single threaded version, false: use multi threaded version, default false
 	NCPUs                   int           // From SDS_NCPUS, set to override number of CPUs to run, this overwrites SDS_ST, default 0 (which means do not use it)
 	CtxOut                  bool          // From SDS_CTXOUT output all context data (this struct), default false
@@ -116,7 +116,7 @@ func (ctx *Ctx) Init() {
 	}
 	// MaxRetry
 	if os.Getenv("SDS_MAXRETRY") == "" {
-		ctx.MaxRetry = 2
+		ctx.MaxRetry = 0
 	} else {
 		maxRetry, err := strconv.Atoi(os.Getenv("SDS_MAXRETRY"))
 		FatalNoLog(err)
