84651a704f9297c337dfc1b65feeea7c7763d14e
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-05-21 13:20:06 +0000

    Struggling with tasks
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ fargate/describe_current_task.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+if [ -z "${AWS_PROFILE}" ]
+then
+  echo "$0: you need to specify AWS_PROFILE=..."
+  exit 1
+fi
+if [ -z "${1}" ]
+then
+  echo "$0: you need to specify env as a first argument: test|prod"
+  exit 2
+fi
+if [ -z "${2}" ]
+then
+  echo "$0: you need to specify cluster name as a second argument"
+  exit 3
+fi
+if [ -z "${3}" ]
+then
+  echo "$0: you need to specify task uuid(s) as a third argument"
+  exit 4
+fi
+aws ecs describe-tasks --cluster "${2}-${1}" --tasks "${3}"
