95ae7a0c5ee89186fb75d69f271ec9b466af5aa6
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-05-21 17:12:57 +0000

    EFS struggle - mount-points
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- fargate/create_efs.sh
+++ fargate/create_efs.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# AP=1 - create access point too
 if [ -z "${AWS_PROFILE}" ]
 then
   echo "$0: you need to specify AWS_PROFILE=..."
@@ -11,4 +12,10 @@ then
 fi
 fsid=`aws efs create-file-system --region "${AWS_REGION}" --creation-token sds-efs-volume --performance-mode maxIO --tags "Key=Name,Value=sds-efs-volume" | jq -r '.FileSystemId'`
 echo "FileSystemId: ${fsid}"
-aws efs create-access-point --client-token sds-efs-access-point --tags "Key=Name,Value=sds-efs-access-point" --file-system-id "${fsid}"
+if [ ! -z "${AP}" ]
+then
+  aws efs create-access-point --client-token sds-efs-access-point --tags "Key=Name,Value=sds-efs-access-point" --file-system-id "${fsid}"
+fi
+subnetid=`aws ec2 describe-subnets | jq -r '.Subnets[] | select(.CidrBlock == "10.0.128.0/17") | .SubnetId'`
+sgid=`aws ec2 describe-security-groups | jq -r '.SecurityGroups[] | select(.Description == "SDS security group") | .GroupId'`
+aws efs create-mount-target --file-system-id "${fsid}" --subnet-id "${subnetid}" --security-group "${sgid}" --region "${AWS_REGION}"
--- fargate/list_efs.sh
+++ fargate/list_efs.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+# AP=1 - list access point too
 if [ -z "${AWS_PROFILE}" ]
 then
   echo "$0: you need to specify AWS_PROFILE=..."
   exit 1
 fi
 aws efs describe-file-systems | jq '.FileSystems[] | select(.Name == "sds-efs-volume")'
-aws efs describe-access-points | jq '.AccessPoints[] | select(.Name == "sds-efs-access-point")'
+if [ ! -z "${AP}" ]
+then
+  aws efs describe-access-points | jq '.AccessPoints[] | select(.Name == "sds-efs-access-point")'
+fi
+fsid=`aws efs describe-file-systems | jq -r '.FileSystems[] | select(.Name == "sds-efs-volume") | .FileSystemId'`
+aws efs describe-mount-targets --file-system-id "${fsid}" | jq '.MountTargets[]'
