c6734074de08cacdb309192593f192c766092af3
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-05-21 16:48:47 +0000

    EFS and subnet gateway struggle
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- fargate/create_efs.sh
+++ fargate/create_efs.sh
@@ -9,4 +9,6 @@ then
   echo "$0: you need to specify AWS_REGION=..."
   exit 2
 fi
-aws efs create-file-system --region "${AWS_REGION}" --creation-token sds-efs-volume --performance-mode maxIO --tags "Key=Name,Value=sds-efs-volume"
+fsid=`aws efs create-file-system --region "${AWS_REGION}" --creation-token sds-efs-volume --performance-mode maxIO --tags "Key=Name,Value=sds-efs-volume" | jq -r '.FileSystemId'`
+echo "FileSystemId: ${fsid}"
+aws efs create-access-point --client-token sds-efs-access-point --tags "Key=Name,Value=sds-efs-access-point" --file-system-id "${fsid}"
--- fargate/delete_efs.sh
+++ fargate/delete_efs.sh
@@ -5,4 +5,6 @@ then
   exit 1
 fi
 fsid=`aws efs describe-file-systems | jq -r '.FileSystems[] | select(.Name == "sds-efs-volume") | .FileSystemId'`
+apid=`aws efs describe-access-points | jq -r '.AccessPoints[] | select(.Name == "sds-efs-access-point") | .AccessPointId'`
+aws efs delete-access-point --access-point-id "${apid}"
 aws efs delete-file-system --file-system-id "${fsid}"
