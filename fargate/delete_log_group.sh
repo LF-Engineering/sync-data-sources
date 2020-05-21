f93b40115eb2cad5b77bc1113f4ee4732cbd033a
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-05-21 12:32:34 +0000

    Few logs related commands
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ fargate/delete_log_group.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+if [ -z "${AWS_PROFILE}" ]
+then
+  echo "$0: you need to specify AWS_PROFILE=..."
+  exit 1
+fi
+aws logs delete-log-group --log-group-name sds-logs
---
+++ fargate/list_log_streams.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ -z "${AWS_PROFILE}" ]
+then
+  echo "$0: you need to specify AWS_PROFILE=..."
+  exit 1
+fi
+if [ -z "${1}" ]
+then
+  echo "$0: you need to specify log group name as a first argument: "
+  exit 2
+fi
+aws logs describe-log-streams --log-group-name "${1}" | jq '.logStreams[] | .logStreamName'
