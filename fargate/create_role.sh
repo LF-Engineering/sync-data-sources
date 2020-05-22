428f4121602fbdb13986131f387234b35756466a
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-05-22 05:54:49 +0000

    create_infra.sh script wip
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- fargate/create_role.sh
+++ fargate/create_role.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
+# SS=1 set secret mode
 if [ -z "${AWS_PROFILE}" ]
 then
   echo "${0}: you need to specify AWS_PROFILE=..."
   exit 1
 fi
 cwd=`pwd`
-aws iam create-role --role-name ecsTaskExecutionRole --assume-role-policy-document "file://${cwd}/fargate/ecsTaskExecutionRole.json"
-aws iam attach-role-policy --role-name ecsTaskExecutionRole --policy-arn arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy
+arn=`aws iam create-role --role-name ecsTaskExecutionRole --assume-role-policy-document "file://${cwd}/fargate/ecsTaskExecutionRole.json" | jq -r '.Role.Arn'`
+echo "Role Arn: ${arn}"
+if [ ! -z "${arn}" ]
+then
+  aws iam attach-role-policy --role-name ecsTaskExecutionRole --policy-arn 'arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy' || exit 3
+  if [ ! -z "${SS}" ]
+  then
+    echo -n "${arn}" > "helm-charts/sds-helm/sds-helm/secrets/SDS_ROLE_ARN.secret"
+  fi
+else
+  echo 'Failed to create role'
+  exit 2
+fi
