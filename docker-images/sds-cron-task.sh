9cdfd355a67dda1ac8cdbc39dcd30274adbe8cd5
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-06-11 09:02:22 +0000

    Cron task for EC2 instance
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ docker-images/sds-cron-task.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+date
+cd /root/go/src/github.com/LF-Engineering/sync-data-sources/ || exit 1
+lock_file=/tmp/sds.lock
+function cleanup {
+  rm -f "${lock_file}"
+}
+if [ -f "${lock_file}" ]
+then
+  echo "$0: another SDS instance is still running, exiting"
+  exit 2
+fi
+> "${lock_file}"
+trap cleanup EXIT
+DRY=1 SDS_NCPUS_SCALE=2 SDS_ES_BULKSIZE=100 SDS_SKIP_P2O=1 ./docker-images/run_sds.sh prod
