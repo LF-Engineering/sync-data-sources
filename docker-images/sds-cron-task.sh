edbc6dfc9e7837443c688ca8ac8e62b99b969be9
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-06-12 09:36:58 +0000

    Update
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- docker-images/sds-cron-task.sh
+++ docker-images/sds-cron-task.sh
@@ -5,7 +5,12 @@ then
   echo "$0: you need to specify name for this run, can be 'sds1' for example"
   exit 1
 fi
-cd /root/go/src/github.com/LF-Engineering/sync-data-sources/ || exit 2
+if [ -z "$2" ]
+then
+  echo "$0: you need to specify environment as a 2nd arg: test|prod"
+  exit 2
+fi
+cd /root/go/src/github.com/LF-Engineering/sync-data-sources/ || exit 3
 lock_file="/tmp/$1.lock"
 function cleanup {
   rm -f "${lock_file}"
@@ -13,8 +18,8 @@ function cleanup {
 if [ -f "${lock_file}" ]
 then
   # echo "$0: another SDS instance \"$1\" is still running, exiting"
-  exit 3
+  exit 4
 fi
 > "${lock_file}"
 trap cleanup EXIT
-./docker-images/run_sds.sh prod
+./docker-images/run_sds.sh "$2"
