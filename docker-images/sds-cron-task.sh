3e490d84dd53ec2daad0c3fd520c20be7a8fbad2
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-06-12 09:27:07 +0000

    Support splitting nasty tasks
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- docker-images/sds-cron-task.sh
+++ docker-images/sds-cron-task.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 date
-cd /root/go/src/github.com/LF-Engineering/sync-data-sources/ || exit 1
-lock_file=/tmp/sds.lock
+if [ -z "$1" ]
+then
+  echo "$0: you need to specify name for this run, can be 'sds1' for example"
+  exit 1
+fi
+cd /root/go/src/github.com/LF-Engineering/sync-data-sources/ || exit 2
+lock_file="/tmp/$1.lock"
 function cleanup {
   rm -f "${lock_file}"
 }
 if [ -f "${lock_file}" ]
 then
-  # echo "$0: another SDS instance is still running, exiting"
-  exit 2
+  # echo "$0: another SDS instance \"$1\" is still running, exiting"
+  exit 3
 fi
 > "${lock_file}"
 trap cleanup EXIT
-SDS_NCPUS_SCALE=2 SDS_ES_BULKSIZE=100 ./docker-images/run_sds.sh prod
+./docker-images/run_sds.sh prod
