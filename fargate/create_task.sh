74dedb82c7736ceb0dcf02161a53c311a6a43f01
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-06-27 07:02:47 +0000

    Support skip/dry run allow copy index functionality
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- fargate/create_task.sh
+++ fargate/create_task.sh
@@ -54,6 +54,7 @@ then
   export SDS_SKIP_MERGE=1
   export SDS_SKIP_HIDE_EMAILS=1
   export SDS_SKIP_ORG_MAP=1
+  export SDS_SKIP_COPY_FROM=1
   export SDS_SKIP_P2O=1
   export SDS_DRY_RUN=1
   #export SDS_RUN_DET_AFF_RANGE=1
@@ -74,6 +75,7 @@ then
   #export SDS_DRY_RUN_ALLOW_HIDE_EMAILS=1
   #export SDS_DRY_RUN_ALLOW_ORG_MAP=1
   #export SDS_DRY_RUN_ALLOW_DET_AFF_RANGE=1
+  #export SDS_DRY_RUN_ALLOW_COPY_FROM=1
   #export SDS_DRY_RUN_ALLOW_SSAW=1
   #export SDS_ONLY_VALIDATE=1
   #export SDS_ONLY_P2O=1
