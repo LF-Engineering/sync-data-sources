9f9396a85aec0fe1d884823b00cd355b6491d393
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-07-14 08:55:46 +0000

    Finished gerrit org support
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- sources/gerrit.go
+++ sources/gerrit.go
@@ -14,58 +14,67 @@ func GetGerritRepos(ctx *Ctx, gerritURL string) (projects, repos []string, err e
 	if ctx.Debug >= 0 {
 		Printf("GetGerritRepos(%s)\n", gerritURL)
 	}
-	partial := "r"
-	method := Get
-	if !strings.HasSuffix(gerritURL, "/") {
-		gerritURL += "/"
-	}
-	url := gerritURL + partial + "/projects/"
-	req, err := http.NewRequest(method, os.ExpandEnv(url), nil)
-	if err != nil {
-		Printf("new request error: %+v for %s url: %s", err, method, url)
-		return
-	}
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		Printf("do request error: %+v for %s url: %s\n", err, method, url)
-		return
-	}
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		Printf("ReadAll request error: %+v for %s url: %s\n", err, method, url)
-		return
-	}
-	_ = resp.Body.Close()
-	if resp.StatusCode != 200 {
-		err = fmt.Errorf("Method:%s url:%s status:%d\n%s", method, url, resp.StatusCode, body)
-		return
-	}
-	var (
-		i int
-		b byte
-	)
-	jsonStart := []byte("{")[0]
-	for i, b = range body {
-		if b == jsonStart {
-			break
+	partials := []string{"r", "gerrit"}
+	for _, partial := range partials {
+		method := Get
+		if !strings.HasSuffix(gerritURL, "/") {
+			gerritURL += "/"
 		}
-	}
-	body = body[i:]
-	var result map[string]interface{}
-	err = json.Unmarshal(body, &result)
-	if err != nil {
-		Printf("Bulk result unmarshal error: %+v", err)
-		return
-	}
-	for project := range result {
-		if !strings.Contains(project, "/") {
+		url := gerritURL + partial + "/projects/"
+		var req *http.Request
+		req, err = http.NewRequest(method, os.ExpandEnv(url), nil)
+		if err != nil {
+			Printf("new request error: %+v for %s url: %s", err, method, url)
+			return
+		}
+		var resp *http.Response
+		resp, err = http.DefaultClient.Do(req)
+		if err != nil {
+			Printf("do request error: %+v for %s url: %s\n", err, method, url)
+			return
+		}
+		var body []byte
+		body, err = ioutil.ReadAll(resp.Body)
+		if err != nil {
+			Printf("ReadAll request error: %+v for %s url: %s\n", err, method, url)
+			return
+		}
+		_ = resp.Body.Close()
+		if resp.StatusCode == 404 {
 			continue
 		}
-		ary := strings.Split(project, "/")
-		org := ary[0]
-		endpoint := gerritURL + partial + "/" + project
-		projects = append(projects, org)
-		repos = append(repos, endpoint)
+		if resp.StatusCode != 200 {
+			err = fmt.Errorf("Method:%s url:%s status:%d\n%s", method, url, resp.StatusCode, body)
+			return
+		}
+		var (
+			i int
+			b byte
+		)
+		jsonStart := []byte("{")[0]
+		for i, b = range body {
+			if b == jsonStart {
+				break
+			}
+		}
+		body = body[i:]
+		var result map[string]interface{}
+		err = json.Unmarshal(body, &result)
+		if err != nil {
+			Printf("Bulk result unmarshal error: %+v", err)
+			return
+		}
+		for project := range result {
+			if project == "All-Projects" || project == "All-Users" {
+				continue
+			}
+			ary := strings.Split(project, "/")
+			org := ary[0]
+			endpoint := gerritURL + partial + "/" + project
+			projects = append(projects, org)
+			repos = append(repos, endpoint)
+		}
+		break
 	}
 	return
 }
