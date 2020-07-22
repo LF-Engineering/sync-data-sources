3693329a8689706cc10d316e0bd0cf7133a88de7
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-07-22 05:27:47 +0000

    Built base image as shared team user 'dajohn'
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- docker-images/base/build_minimal.sh
+++ docker-images/base/build_minimal.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ -z "${DOCKER_USER}" ]
 then
-  export DOCKER_USER=lukaszgryglicki
+  export DOCKER_USER=dajohn
 fi
 ./collect_and_build.sh && docker build -f Dockerfile -t "${DOCKER_USER}/dev-analytics-grimoire-docker-minimal" . && docker push "${DOCKER_USER}/dev-analytics-grimoire-docker-minimal"
--- docker-images/base/test_image.sh
+++ docker-images/base/test_image.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ -z "${DOCKER_USER}" ]
 then
-  export DOCKER_USER=lukaszgryglicki
+  export DOCKER_USER=dajohn
 fi
 docker run -it "${DOCKER_USER}/dev-analytics-grimoire-docker-minimal" /bin/bash
