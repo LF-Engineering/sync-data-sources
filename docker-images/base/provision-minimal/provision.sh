e72c7ca1f8bed47db173e01bb48bd60edc6c0f1e
lfpratik <pratikk@proximabiz.com>
2020-12-12 03:31:09 +0530

    grimoirelab-elk stack build issues fixed

--- docker-images/base/provision-minimal/provision.sh
+++ docker-images/base/provision-minimal/provision.sh
@@ -3,7 +3,7 @@ set -eo pipefail
 export SETUPTOOLS_USE_DISTUTILS=stdlib
 # export PIP_USE_FEATURE=2020-resolver
 pip3 install --upgrade setuptools
-pip3 install --upgrade pip
+pip3 install --upgrade "pip<=20.2.4"
 pip3 install emoji
 pip3 install --upgrade requests
 
@@ -18,5 +18,7 @@ done
 pip3 install geopy==2.0.0
 pip3 install six==1.12.0
 pip3 install "feedparser>=5.1.3,<6.0.0"
+pip3 install "sqlalchemy<1.4,>=1.2"
+pip3 install PyMySQL==0.9.3
 
 cp repos/cloc/cloc /usr/bin/
