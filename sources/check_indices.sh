2ff3a9eab0e50bbc2abe6a0fd198f9b54c097aa6
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2021-01-20 09:49:11 +0000

    Update FINOS report script
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- sources/check_indices.sh
+++ sources/check_indices.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-clear; curl -s http://127.0.0.1:19200/_cat/aliases | sort; curl -s http://127.0.0.1:19200/_cat/indices | sort
-for f in `cat idx`; do echo "index $f"; curl -s "http://127.0.0.1:19200/$f/_search?size=10000" | jq '.hits.hits[]._source.github_repo' | sort | uniq; done > out
+clear; curl -s http://127.0.0.1:19200/_cat/aliases | sort > ~/idx; curl -s http://127.0.0.1:19200/_cat/indices | sort >> ~/idx; cat ~/idx
+#for f in `cat idx`; do echo "index $f"; curl -s "http://127.0.0.1:19200/$f/_search?size=100000" | jq '.hits.hits[]._source.github_repo' | sort | uniq; done > out
+for f in `cat ~/idx`; do echo "index $f"; curl -s "http://127.0.0.1:19200/$f/_search?size=100000" | jq '.hits.hits[]._source.origin' | sort | uniq; done > ~/report.txt
