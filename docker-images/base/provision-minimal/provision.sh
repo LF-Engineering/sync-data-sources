22d0361b2142a8f5e98c0fa29cd3e2129f48f0a1
lfpratik <pratikk@proximabiz.com>
2021-01-22 23:17:21 +0530

    Fixed SDS build issue for insights

--- docker-images/base/provision-minimal/provision.sh
+++ docker-images/base/provision-minimal/provision.sh
@@ -2,24 +2,25 @@
 set -eo pipefail
 export SETUPTOOLS_USE_DISTUTILS=stdlib
 # export PIP_USE_FEATURE=2020-resolver
-pip3 install --upgrade setuptools
-pip3 install --upgrade pip
-pip3 install emoji
-pip3 install --upgrade requests
+python3 -m pip install --upgrade setuptools
+python3 -m pip install --upgrade pip
+python3 -m pip install emoji
+python3 -m pip install --upgrade requests
 
-repos=(grimoirelab-perceval grimoirelab-elk grimoirelab-sortinghat grimoirelab-kingarthur)
+repos=(grimoirelab-elk grimoirelab-sortinghat grimoirelab-kingarthur grimoirelab-perceval)
 
 for r in "${repos[@]}"; do 
   echo "INSTALLING ${r}"
-  # python3 -m pip install -e "${REPOS_DIR}/$r";
-  pip3 install -e "${REPOS_DIR}/$r";
+  python3 -m pip install -e "${REPOS_DIR}/$r";
+  # pip3 install -e "${REPOS_DIR}/$r";
 done
 
-pip3 install geopy==2.0.0
-pip3 install six==1.12.0
-pip3 install "feedparser>=5.1.3,<6.0.0"
-pip3 install "sqlalchemy<1.4,>=1.2"
-pip3 install PyMySQL==0.9.3
-pip3 install PyJWT>=1.7.1
+python3 -m pip install geopy==2.0.0
+python3 -m pip install six==1.12.0
+python3 -m pip install "feedparser>=5.1.3,<6.0.0"
+python3 -m pip install "sqlalchemy<1.4,>=1.2"
+python3 -m pip install PyMySQL==0.9.3
+python3 -m pip install PyJWT>=1.7.1
+python3 -m pip install urllib3==1.24.3
 
 cp repos/cloc/cloc /usr/bin/
