40aa499f059326489cc11d12caaf8015e066b467
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2021-01-28 13:28:34 +0000

    Script to confirm tha FINOS metadata import worked fine
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ sources/check_metadata.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+ES="`cat ../helm-charts/sds-helm/sds-helm/secrets/ES_URL.prod.secret`"
+for f in `cat md_indices.secret`
+do
+  if [[ ${f} == *"jira"* ]]
+  then
+    echo "jira index: $f"
+    curl -s -XPOST -H 'Content-type: application/json' "${ES}/_sql?format=csv" -d"{\"query\":\"select workinggroup from \\\"${f}\\\" group by workinggroup order by workinggroup\"}"
+  elif [[ ${f} == *"finosmeetings"* ]]
+  then
+    echo "finosmeetings index: $f"
+    curl -s -XPOST -H 'Content-type: application/json' "${ES}/_sql?format=csv" -d"{\"query\":\"select workinggroup from \\\"${f}\\\" group by workinggroup order by workinggroup\"}"
+  else
+    echo "index: $f"
+    curl -s -XPOST -H 'Content-type: application/json' "${ES}/_sql?format=txt" -d"{\"query\":\"select workinggroup, meta_title, meta_type, meta_program, meta_contributed, meta_formed, meta_state from \\\"${f}\\\" group by workinggroup, meta_title, meta_type, meta_program, meta_contributed, meta_formed, meta_state order by workinggroup, meta_title, meta_type, meta_program, meta_contributed, meta_formed, meta_state\"}"
+  fi
+  echo "======================="
+done
