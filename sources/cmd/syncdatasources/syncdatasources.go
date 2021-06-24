d921157830a4bc6f795a85661686ae926303e967
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2021-06-24 10:09:23 +0000

    Fix pp
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- sources/cmd/syncdatasources/syncdatasources.go
+++ sources/cmd/syncdatasources/syncdatasources.go
@@ -86,7 +86,7 @@ var (
 			"DA_GIT_NCPUS":            "8",
 			"DA_GIT_DEBUG":            "1",
 			"DA_GIT_RETRY":            "4",
-			"DA_GIT_PAIR_PROGRAMMING": "false",
+			//"DA_GIT_PAIR_PROGRAMMING": "false",
 		},
 		lib.GitHub: {
 			///"DA_GITHUB_LEGACY_UUID":   "1",
@@ -7026,6 +7026,9 @@ func processTask(ch chan lib.TaskResult, ctx *lib.Ctx, idx int, task lib.Task, a
 	if dads {
 		for k, v := range epEnv {
 			mainEnv[k] = v
+		}
+		if task.PairProgramming {
+			mainEnv[envPrefix+"PAIR_PROGRAMMING"] = "1"
 		}
 		switch ctx.CmdDebug {
 		case 0:
