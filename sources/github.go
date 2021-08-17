83adb1b3ffa9e5bd2e6900bfee51f8e2ebc39c59
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2021-08-17 04:20:38 +0000

    Check this
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- sources/github.go
+++ sources/github.go
@@ -6,8 +6,8 @@ import (
 	"strings"
 	"time"
 
-	// "github.com/google/go-github/v33/github" // with go mod enabled
-	"github.com/google/go-github/github" // with go mod disabled
+	"github.com/google/go-github/v38/github" // with go mod enabled
+	// "github.com/google/go-github/github" // with go mod disabled
 	"golang.org/x/oauth2"
 )
 
